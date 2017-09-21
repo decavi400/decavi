29f47326afe819e1902ac9f1b0cefee4f41e3806
decavi <decavi400@hotmail.com>
2017-09-21 16:41:30 +0800

    y

--- ntnu_mvc_test/Controllers/HomeController.cs
+++ ntnu_mvc_test/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace ntnu_mvc_test.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            return View();//
         }
 
         public ActionResult About()

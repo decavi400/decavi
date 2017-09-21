9a64a3d148dba4e8cb8c9366eb27a3a8be53d68f
decavi <decavi400@hotmail.com>
2017-09-21 17:13:21 +0800

    123

--- ntnu_mvc_test/Controllers/HomeController.cs
+++ ntnu_mvc_test/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace ntnu_mvc_test.Controllers
             ViewBag.Message = "Your application description page.";
 
             return View();
-            //yee
+            //yeeeee
         }
 
         public ActionResult Contact()
